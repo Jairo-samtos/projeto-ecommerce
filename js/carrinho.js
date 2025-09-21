dc66617a4833b3b7ce53547d2956f4622cb61565
Jairo Santos <jairosamtos@gmail.com>
2025-09-21 16:39:31 -0300

    atualiza contador do carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -53,6 +53,7 @@ botoesAdicionarAoCarrinho.forEach(botao => {
         }
 
         salvarProdutosNoCarrinho(carrinho);
+        atualizarContadorDoCarrinho();
 
     });
 });
@@ -65,3 +66,17 @@ function obterProdutosDoCarrinho() {
     const produtos = localStorage.getItem('carrinho');
     return produtos ? JSON.parse(produtos) : [];
 }
+
+// passo 4 - atualizar o contador do carrinho de compras
+function atualizarContadorDoCarrinho() {
+    const carrinho = obterProdutosDoCarrinho();
+    let total = 0;
+
+    carrinho.forEach(produto => {
+        total += produto.quantidade;
+    });
+
+    document.getElementById('contador-carrinho').textContent = total;
+}
+
+atualizarContadorDoCarrinho();
